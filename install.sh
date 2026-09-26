e2881ed96e8d23f6d3364e7f281a47fb718efa52
Artem Balashow <pockerhead@ya.ru>
2026-09-26 05:54:11 +0300

    task: TASK-044 and TASK-048 done; release v0.1.4

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ set -eu
 REPO=pockerhead/cctg
 # The release this script belongs to. Bumped in the commit that gets the
 # tag; release.yml refuses a tag that differs.
-RELEASE=v0.1.3
+RELEASE=v0.1.4
 # Marks every wrapper this script writes; --uninstall removes only those.
 MARK=cctg-install
 # device.env keys this script sets; other lines of the file are kept.
